8fe9154d43822545b40320948a6824cfe5c8cced
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-09 15:59:50 +0100

    Add DAL Servcie

---
+++ DemoUser.DAL/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using DemoUser.DAL.Entities;
+using DemoUser.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoUser.DAL.Services
+{
+    public class UserService : IUserRepository<User>
+    {
+        public Guid CheckPassword(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Insert(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
