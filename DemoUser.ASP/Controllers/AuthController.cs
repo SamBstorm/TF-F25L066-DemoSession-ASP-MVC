505a12d07befea9b6ac56a280b513e4c491665f0
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-11 10:23:40 +0100

    Add Services into ASP + Add AuthController

---
+++ DemoUser.ASP/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using DemoUser.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUser.ASP.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IUserRepository<BLL.Entities.User> _service;
+
+        public AuthController(IUserRepository<BLL.Entities.User> service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(FormCollection form)
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout(FormCollection form)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(FormCollection form)
+        {
+            return View();
+        }
+    }
+}
--- DemoUser.ASP/Program.cs
+++ DemoUser.ASP/Program.cs
@@ -1,3 +1,11 @@
+using DemoUser.Domain.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+using BLLEnt = DemoUser.BLL.Entities;
+using BLLServ = DemoUser.BLL.Services;
+using DALEnt = DemoUser.DAL.Entities;
+using DALServ = DemoUser.DAL.Services;
+
 namespace DemoUser.ASP
 {
     public class Program
@@ -5,9 +13,20 @@ namespace DemoUser.ASP
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            //Appelle du fichier de configuration appsettings.json
+            IConfiguration configuration = builder.Configuration;
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+
+            //Services d'intégration des couches BLL et DAL
+            builder.Services.AddScoped<DbConnection>(serviceProvider => new SqlConnection(
+                configuration.GetConnectionString("DemoUser.Database")));
+            //Intégration des services de la BLL
+            builder.Services.AddScoped<IUserRepository<BLLEnt.User>, BLLServ.UserService>();
+            //Intégration des services de la DAL
+            builder.Services.AddScoped<IUserRepository<DALEnt.User>, DALServ.UserService>();
+
 
             var app = builder.Build();
 
