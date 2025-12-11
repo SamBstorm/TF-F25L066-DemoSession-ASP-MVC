26ce638550201cae745d979aaacabffde968bccf
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-11 11:28:16 +0100

    Fix UserRegisterForm

--- DemoUser.ASP/Models/UserRegisterForm.cs
+++ DemoUser.ASP/Models/UserRegisterForm.cs
@@ -5,15 +5,18 @@ namespace DemoUser.ASP.Models
 {
     public class UserRegisterForm
     {
+        [DisplayName("Adresse email : ")]
         [Required(ErrorMessage = "L'adresse e-mail est obligatoire.")]
         [EmailAddress]
         public string Email { get; set; }
+        [DisplayName("Mot de passe : ")]
         [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
-        [PasswordPropertyText]
-        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\-\.=+$@µ£_!#])$.{8,64}", ErrorMessage = "Le mot de passe doit contenir au minimum 1 minuscule, 1 majuscule, 1 chiffre et 1 symbole (-.=+$@µ£_!#) et être composé de 8 à 64 caractères.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\-\.=+$@µ£_!#]).{8,64}$", ErrorMessage = "Le mot de passe doit contenir au minimum 1 minuscule, 1 majuscule, 1 chiffre et 1 symbole (-.=+$@µ£_!#) et être composé de 8 à 64 caractères.")]
         public string Password { get; set; }
+        [DisplayName("Confirmation du mot de passe : ")]
         [Required(ErrorMessage = "Veuillez confirmer le mot de passe.")]
-        [PasswordPropertyText]
+        [DataType(DataType.Password)]
         [Compare(nameof(Password), ErrorMessage = "La confirmation ne correspond pas au mot de passe.")]
         public string ConfirmPassword { get; set; }
     }
